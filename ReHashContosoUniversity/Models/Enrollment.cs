895bfe45c0472a4ce4b959073f84390bfc91c3b7
daniel-anderson <daniel.anderson28562@hotmail.com>
2015-02-25 16:56:13 -0500

    Initial commit

---
+++ ReHashContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ReHashContosoUniversity.Models
+{
+
+	public enum Grade
+	{
+		A = 1, B = 2, C = 3, D = 4, F = 5
+	}
+
+	public class Enrollment
+	{
+
+		public int EnrollmentID { get; set; }
+		public int CourseID { get; set; }
+		public int StudentID { get; set; }
+		public Grade? Grade { get; set; }
+
+		public virtual Course Course { get; set; }
+		public virtual Student Student { get; set; }
+
+	}
+}
