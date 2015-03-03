ac6825834676b7dcae50b10eac0a382f8df57071
daniel-anderson <daniel.anderson28562@hotmail.com>
2015-03-03 13:22:13 -0500

    Updated the HTTP Post Edit action within the Student Controller.
    Renamed the method given the signature conflict with another method and added an Attribute to indicate that it should still handle the Edit action.
    
    Also adjusted the updating process so that it depends more on the actual Model as opposed to an arbitrary set of strings that supposedly match the Model.

--- ReHashContosoUniversity/Controllers/StudentController.cs
+++ ReHashContosoUniversity/Controllers/StudentController.cs
@@ -83,21 +83,41 @@ namespace ReHashContosoUniversity.Controllers
             return View(student);
         }
 
+
         // POST: Student/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        public ActionResult EditPost(int? id)
         {
-            if (ModelState.IsValid)
-            {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(student);
+
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+
+			var studentToUpdate = db.Students.Find(id);
+
+			if (TryUpdateModel(studentToUpdate, "", new string [] { "LastName", "FirstMidName", "EnrollmentDate"}))
+			{
+				try
+				{
+					db.Entry(studentToUpdate).State = EntityState.Modified;
+					db.SaveChanges();
+
+					return RedirectToAction("Index");
+				}
+				catch (DataException)
+				{
+					ModelState.AddModelError("", "Enable to save changes.  Try again, and if the problem persists, see your system administrator.");
+				}
+			}
+
+			return View(studentToUpdate);
+
         }
+
 
         // GET: Student/Delete/5
         public ActionResult Delete(int? id)
