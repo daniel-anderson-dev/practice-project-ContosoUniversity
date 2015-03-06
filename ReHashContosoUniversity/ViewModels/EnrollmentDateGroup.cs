bbda0892de9a60b86860bc8251afceeb1ab4681f
daniel-anderson <daniel.anderson28562@hotmail.com>
2015-03-06 13:36:44 -0500

    Added a ViewModel representing a custom "View" of data that is then displayed within the Home > About page.  The data is a "Group By" of Students by Enrollment Date.

---
+++ ReHashContosoUniversity/ViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ReHashContosoUniversity.ViewModels
+{
+	public class EnrollmentDateGroup
+	{
+
+		[DataType(DataType.Date)]
+		public DateTime? EnrollmentDate { get; set; }
+
+		public int StudentCount { get; set; }
+
+	}
+}
