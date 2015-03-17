9eeaab2a41d6b716b803298ec3f0acc644f6ab05
daniel-anderson <daniel.anderson28562@hotmail.com>
2015-03-17 16:53:01 -0400

    Added some rudimentary DataAnnotations to the Student Model.

--- ReHashContosoUniversity/Models/Student.cs
+++ ReHashContosoUniversity/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace ReHashContosoUniversity.Models
@@ -8,8 +9,12 @@ namespace ReHashContosoUniversity.Models
 	{
 
 		public int ID { get; set; }
+		[StringLength(50, ErrorMessage = "Last Name cannot be more than 50 characters.")]
 		public string LastName { get; set; }
+		[StringLength(50, ErrorMessage = "First Name cannot be more than 50 characters.")]
 		public string FirstMidName { get; set; }
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
 		public DateTime EnrollmentDate { get; set; }
 
 		public virtual ICollection<Enrollment> Enrollments { get; set; }
