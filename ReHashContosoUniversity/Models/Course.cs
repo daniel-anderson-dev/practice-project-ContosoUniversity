6ad0d9902a367ca36f076654b1a2d10b90ab8192
daniel-anderson <daniel.anderson28562@hotmail.com>
2015-03-18 17:24:39 -0400

    Created the Course Model.

--- ReHashContosoUniversity/Models/Course.cs
+++ ReHashContosoUniversity/Models/Course.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 
@@ -9,11 +9,20 @@ namespace ReHashContosoUniversity.Models
 	{
 
 		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		[Display(Name = "Number")]
 		public int CourseID { get; set; }
+
+		[StringLength(50, MinimumLength = 3)]
 		public string Title { get; set; }
+
+		[Range(0, 5)]
 		public int Credits { get; set; }
 
+		public int DepartmentID { get; set; }
+
+		public virtual Department Department { get; set; }
 		public virtual ICollection<Enrollment> Enrollments { get; set; }
-		
+		public virtual ICollection<Instructor> Instructors { get; set; }
+				
 	}
 }
